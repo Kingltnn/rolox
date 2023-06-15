04a6c45cb0c46f4f1b035bd6d1edb940e9280770
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2023-06-15 19:02:02 +0700

    Update teledia.lua

--- rolox/teledia.lua
+++ rolox/teledia.lua
@@ -1,21 +1,5 @@
 task.wait(100)
-local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
-local Library = require(game:GetService("ReplicatedStorage").Library)
-local Fire, Invoke = Network.Fire, Network.Invoke
-
-function TeleportArea(World, Area)
-	Library.WorldCmds.Load(World)
-	if Area ~= "" or Area ~= nil then
-		local areaTeleport = Library.WorldCmds.GetMap().Teleports:FindFirstChild(Area)
-		if areaTeleport then
-			local Humanoid = game.Players.LocalPlayer.Character.Humanoid
-			local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
-			game.Players.LocalPlayer.Character:PivotTo(areaTeleport.CFrame + areaTeleport.CFrame.UpVector * (Humanoid.HipHeight + HumanoidRootPart.Size.Y / 2))
-		end
-	end
-end
-
-TeleportArea("Diamond Mine", "Mystic Mine")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kingltnn/rolox/main/rolox/hopnew.lua"))()
 wait(30)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Kingltnn/rolox/main/rolox/hophop.lua"))()
 
