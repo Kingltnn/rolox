9a94038c6003ee699b2173fe7e31f9599c153854
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2023-06-15 19:38:11 +0700

    Update teledia.lua

--- rolox/teledia.lua
+++ rolox/teledia.lua
@@ -1,9 +1,8 @@
-task.wait(50)
-local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
-local Library = require(game:GetService("ReplicatedStorage").Library)
-repeat
+task.wait(30)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Kingltnn/rolox/main/rolox/hopnew.lua"))()
-  until oldJob ~= game.JobId
+
 wait(30)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Kingltnn/rolox/main/rolox/hophop.lua"))()
 
