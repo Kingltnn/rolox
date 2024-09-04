1ceb7260968f84c699c86fb2e9113dc316a93c9f
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2024-09-04 11:01:43 +0700

    Update kittyware.lua

--- kittyware.lua
+++ kittyware.lua
@@ -1,13 +1,26 @@
 getgenv().KiTTYWARE_ColorWar = {
     Farm_Zones = true,
+    Farming_Misc = {
+        Pets_Per_Breakable = 1,
+        Auto_Pickups = true,
+        Inf_Pet_Speed = true,
+
+        Event_Specific = {
+            Color_Gifts = {Enabled = true, Minimum_Buckets = 75},
+            Clan_Points = {Enabled = false, Minimum_Buckets = 50},
+            Stay_Teams = {Enabled = false, Only_Winning = true},
+        },
+    },
     Hatch_Eggs = true,
     Hatching_Misc = {
-        discord_UserID = "kingltn",
+        discord_UserID = "",
         hugeNotif_Webhook = "https://discord.com/api/webhooks/1279722812931575851/G30w5whctavJ7ABeLBE0ZCFgxegLu-PHd9HMex-748vLqJ6tLK-QdicuAiY0Xqm8wRwo",
         hugeNotif_Enabled = true,
         autoMail_User = "Enter your Username.",
-        autoMail_Enabled = false, -- WARNING: using this feature has a .5% Chance to donate to me and Wyan :3
+        autoMail_Enabled = false,
     },
-    Fps_Boost1 = true,
+    StatsGui = true,
+    Fps_Boost1 = true, -- world stuff invisible
+    Fps_Boost2 = false, -- disable localscript 
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/38e4778402a1d51414b40ab60274c5ed.lua"))()
