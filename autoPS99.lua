cd62d8165a811c2475ae085264fd17b6f5fd933a
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2024-09-18 12:36:19 +0700

    Update autoPS99.lua

--- autoPS99.lua
+++ autoPS99.lua
@@ -10,7 +10,7 @@ getgenv().Settings = {
         "kenchuboy2k",
         "kingltntest",
     },
-    ["Split Items Evenly"] = true, --// False -> it will send the Amount per account.
-    ["Only Online Accounts"] = true,
+    ["Split Items Evenly"] = false, --// False -> it will send the Amount per account.
+    ["Only Online Accounts"] = false,
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/86847850c3165379f5be2d9d071eaccb.lua"))()
